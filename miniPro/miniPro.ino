d4f84520499baaca83374b4b976fca3e30f8084c
Phạm Thanh Tùng <tungpham010203@gmail.com>
2023-06-25 14:43:14 +0700

    lần 1

--- miniPro/miniPro.ino
+++ miniPro/miniPro.ino
@@ -3,23 +3,23 @@
 *phát tín hiệu khi đến giao lộ
 */
 // #include <IRremote.h>
-#include <IRremote.h>
+// #include <IRremote.h>
 
-#define IR_LED_PIN 3
+// #define IR_LED_PIN 3
 
-IRsend irsend(IR_LED_PIN);
+// IRsend irsend(IR_LED_PIN);
 
-void setup()
-{
-  Serial.begin(9600);
-  irsend.enableIROut(38);  // Thiết lập tần số là 38kHz
-}
+// void setup()
+// {
+//   Serial.begin(9600);
+//   irsend.enableIROut(38);  // Thiết lập tần số là 38kHz
+// }
 
-void loop()
-{
-  irsend.sendSony(64260, 16);  // Gửi mã 64260, 16 bits
-  delay(500);  // Chờ 0.5 giây trước khi gửi mã tiếp theo
-}
+// void loop()
+// {
+//   irsend.sendSony(64260, 16);  // Gửi mã 64260, 16 bits
+//   delay(500);  // Chờ 0.5 giây trước khi gửi mã tiếp theo
+// }
 // #include <IRremote.h>
 
 // #define IR_LED_PIN 9
@@ -34,3 +34,22 @@ void loop()
 //   irsend.sendSony(64260, 20); // Gửi mã 64260 với độ dài 20 bit
 //   delay(1000); // Đợi 1 giây trước khi gửi lại
 // }
+#include <IRremote.h>
+
+#define IR_LED_PIN 10
+
+IRsend irsend(IR_LED_PIN);
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("-------");
+  Serial.println("12345");
+  irsend.sendSony(12345, 32);  // Gửi mã Sony (64260) trong 20 bit
+  delay(500);
+  Serial.println("-------");
+  Serial.println("10101");
+  irsend.sendSony(10101, 32);  // Gửi mã Sony (64260) trong 20 bit
+  delay(500);
+}
--- uno/uno.ino
+++ uno/uno.ino
@@ -3,33 +3,67 @@
 *nhận tín hiệu xe đến giao lộ
 */
 
-#include <IRremote.h>
-const int RECV_PIN = 2;
+// #include <IRremote.h>
+// const int RECV_PIN = 2;
 
-unsigned long lastTime = millis();
-//=================================================
-void setup() {
-  Serial.begin(9600);
-  //Khởi động bộ thu
-  IrReceiver.begin(RECV_PIN, true, 13); 
-  Serial.println("dataRemote");
-}
-//=================================================
-void loop() {
-  if (IrReceiver.decode()) {
-    uint32_t dataRemote = IrReceiver.decodedIRData.decodedRawData;
-    if(dataRemote>0){
-      Serial.println(dataRemote);
-      if (millis() - lastTime > 250) {
-        switch(dataRemote){
-          case 64260://so1
-            Serial.println("20o");
-          break;
+// unsigned long lastTime = millis();
+// //=================================================  
+// void setup() {
+//   Serial.begin(9600);
+//   //Khởi động bộ thu
+//   IrReceiver.begin(RECV_PIN, true, 13); 
+//   Serial.println("dataRemote");
+// }
+// //=================================================
+// void loop() {
+//   if (IrReceiver.decode()) {
+//     uint32_t dataRemote = IrReceiver.decodedIRData.decodedRawData;
+//     if(dataRemote>0){
+//       Serial.println(dataRemote);
+//       if (millis() - lastTime > 250) {
+//         switch(dataRemote){
+//           case 64260://so1
+//             Serial.println("20o");
+//           break;
          
-        }
-      }
-    }
-    lastTime = millis();
-    IrReceiver.resume(); // Cho phép nhận giá trị tiếp theo
+//         }
+//       }
+//     }
+//     lastTime = millis();
+//     IrReceiver.resume(); // Cho phép nhận giá trị tiếp theo
+//   }
+// }
+#include <IRremote.h>
+
+const int IR_RECEIVE_PIN = 2;  // Chân số 2 được sử dụng để kết nối cảm biến IR1838
+
+IRrecv irrecv(IR_RECEIVE_PIN);
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();  // Bắt đầu nhận tín hiệu hồng ngoại
+  Serial.println("------111-");
+}
+
+void loop()
+{
+  // if (IrReceiver.decode()) {
+  //   uint32_t dataRemote = IrReceiver.decodedIRData.decodedRawData;
+  //   if(dataRemote>0){
+  //     Serial.println(dataRemote);}}
+  if (irrecv.decode(&results))
+  {
+    unsigned long value = results.value;
+    Serial.println("-------");
+    Serial.print("Mã nhận được: ");
+    // Serial.println(value, HEX);
+    Serial.println(value, DEC);  // Hiển thị giá trị dưới dạng số thập phân
+    // Serial.println(value, BIN);  // Hiển thị giá trị dưới dạng số nhị phân
+    // Serial.println(value, OCT);  // Hiển thị giá trị dưới dạng số bát phân
+    // Serial.println(value, BYTE); // Hiển thị giá trị dưới dạng số nguyên 8-bit
+
+    irrecv.resume();  // Tiếp tục nhận tín hiệu hồng ngoại
   }
 }
