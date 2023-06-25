f4c135fa71df785576ceec9ea8aa3f3abbef351b
Phạm Thanh Tùng <tungpham010203@gmail.com>
2023-06-25 23:16:30 +0700

    them den

--- miniPro/miniPro.ino
+++ miniPro/miniPro.ino
@@ -2,43 +2,16 @@
 *thu tín hiệu khi sắp có đèn đỏ
 *phát tín hiệu khi đến giao lộ
 */
-// #include <IRremote.h>
-// #include <IRremote.h>
 
-// #define IR_LED_PIN 3
-
-// IRsend irsend(IR_LED_PIN);
-
-// void setup()
-// {
-//   Serial.begin(9600);
-//   irsend.enableIROut(38);  // Thiết lập tần số là 38kHz
-// }
-
-// void loop()
-// {
-//   irsend.sendSony(64260, 16);  // Gửi mã 64260, 16 bits
-//   delay(500);  // Chờ 0.5 giây trước khi gửi mã tiếp theo
-// }
-// #include <IRremote.h>
-
-// #define IR_LED_PIN 9
-
-// IRsend irsend(IR_LED_PIN);
-
-// void setup() {
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   irsend.sendSony(64260, 20); // Gửi mã 64260 với độ dài 20 bit
-//   delay(1000); // Đợi 1 giây trước khi gửi lại
-// }
+unsigned long lastTime = millis();
 #include <IRremote.h>
 
 const int IR_RECEIVE_PIN = 2;    // Pin used to connect the IR receiver module (IR1838)
 const int IR_TRANSMIT_PIN = 10;   // Pin used to connect the IR LED for transmission
-
+const int coi =3;
+const int denXanh=4;
+const int denDo=5;
+const int denVang=6;
 IRrecv irrecv(IR_RECEIVE_PIN);
 IRsend irsend(IR_TRANSMIT_PIN);  // Create an IRsend object for IR transmission
 decode_results results;
@@ -46,23 +19,50 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn();
+  irrecv.enableIRIn();// Bắt đầu nhận tín hiệu hồng ngoại
   Serial.println("IR Receiver initialized");
-  pinMode(IR_TRANSMIT_PIN, OUTPUT);  // Set IR transmit pin as output
-  pinMode(3, OUTPUT);  // Set IR transmit pin as output
-  
+  pinMode(IR_TRANSMIT_PIN, OUTPUT);
+  pinMode(coi, OUTPUT);
+  pinMode(denDo, OUTPUT);
+  pinMode(denXanh, OUTPUT);
+  pinMode(denVang, OUTPUT);
+  digitalWrite(coi, LOW);
+  digitalWrite(denDo, LOW);
+  digitalWrite(denXanh, HIGH);
+  digitalWrite(denVang, LOW);
 }
 
 void loop()
 {
-  digitalWrite(3, 1);
+  
   if (irrecv.decode(&results))
   {
     unsigned long value = results.value;
-    Serial.println(value, DEC);
+    // Serial.println(value, DEC);
+    Serial.println(value);
     irrecv.resume();
-  }//Giải quyết dòng sau để truyền lại mã IR đã nhận được
-    irsend.sendSony(111, 20);
-    delay(600);
+    if (value=4321&&millis()-lastTime>3000)
+    {
+      digitalWrite(coi, 1);
+      digitalWrite(denDo, 1);
+      digitalWrite(denXanh, 0);
+      digitalWrite(denVang, 0);
+      lastTime=millis();
+    } 
+  }
+  irsend.sendSony(1111, 20);
+  delay(600);
+  if (millis() - lastTime > 500)
+  {
+    digitalWrite(coi, 0); 
+  }
+  if (millis() - lastTime > 2000)
+  {
+    digitalWrite(coi, 0);
+    digitalWrite(denDo, 0);
+    digitalWrite(denXanh, 1);
+    digitalWrite(denVang, 0);
+  }
+  
 }
 
--- uno/uno.ino
+++ uno/uno.ino
@@ -34,36 +34,118 @@
 //   }
 // }
 #include <IRremote.h>
+unsigned long lastTime = millis();
+#include <IRremote.h>
+const int IR_RECEIVE_PIN01 = 2; //thu
+const int IR_TRANSMIT_PIN01 = 3; //phast
+IRrecv irrecv(IR_RECEIVE_PIN01);
+IRsend irsend(IR_TRANSMIT_PIN01);  // Create an IRsend object for IR transmission
 
-const int IR_RECEIVE_PIN = 2;  // Chân số 2 được sử dụng để kết nối cảm biến IR1838
-
-IRrecv irrecv(IR_RECEIVE_PIN);
+const int IR_RECEIVE_PIN02 = 4; //thu
+const int IR_TRANSMIT_PIN02 = 5; //phast
+IRrecv irrecv02(IR_RECEIVE_PIN02);
+IRsend irsend02(IR_TRANSMIT_PIN02); 
 decode_results results;
 
+const int denXanh1=6;
+const int denVang1=7;
+const int denDo1=8;
+const int denXanh2=9;
+const int denVang2=10;
+const int denDo2=11;
+
+const int timeXanh1=10000/2;   //1s=1000mls         //
+const int timeXanh2=10000/2;
+const int timeVang=3000/2;
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn();  // Bắt đầu nhận tín hiệu hồng ngoại
-  Serial.println("------111-");
+  // irrecv.enableIRIn();// Bắt đầu nhận tín hiệu hồng ngoại
+  Serial.println("IR Receiver initialized");
+  pinMode(IR_TRANSMIT_PIN01, OUTPUT);
+  pinMode(IR_TRANSMIT_PIN02, OUTPUT);
+
+  pinMode(denDo1, OUTPUT);
+  pinMode(denXanh1, OUTPUT);
+  pinMode(denVang1, OUTPUT);
+
+  digitalWrite(denDo1, LOW);
+  digitalWrite(denXanh1, HIGH);
+  digitalWrite(denVang1, LOW);
+}
+void xanh1(){
+  //xanh dai on-đỏ ngắn on
+  Serial.println("xanh1");
+  digitalWrite( denXanh1, HIGH); 
+  digitalWrite( denVang1,LOW); 
+  digitalWrite( denDo1, LOW); 
+  digitalWrite( denXanh2, LOW); 
+  digitalWrite( denVang2, LOW); 
+  digitalWrite( denDo2, HIGH);
+}
+void vang1(){
+  //vang dài on- dỏ ngắn on    
+  Serial.println("vang1");                   
+  digitalWrite( denXanh1, LOW); 
+  digitalWrite( denVang1,HIGH); 
+  digitalWrite( denDo1, LOW); 
+  digitalWrite( denXanh2, LOW); 
+  digitalWrite( denVang2, LOW); 
+  digitalWrite( denDo2, HIGH);
 }
 
+void xanh2(){
+  //xanh ngan on - do dai on  
+  Serial.println("xanh2");                 
+  digitalWrite( denXanh1, LOW); 
+  digitalWrite( denVang1,LOW); 
+  digitalWrite( denDo1, HIGH); 
+  digitalWrite( denXanh2, HIGH); 
+  digitalWrite( denVang2, LOW); 
+  digitalWrite( denDo2,LOW); 
+}
+
+void vang2(){
+  //vang ngan on-do dai on
+  Serial.println("vang 2");
+  digitalWrite( denXanh1, LOW); 
+  digitalWrite( denVang1,LOW); 
+  digitalWrite( denDo1, HIGH); 
+  digitalWrite( denXanh2, LOW); 
+  digitalWrite( denVang2, HIGH); 
+  digitalWrite( denDo2, LOW);
+}
 void loop()
 {
-  // if (IrReceiver.decode()) {
-  //   uint32_t dataRemote = IrReceiver.decodedIRData.decodedRawData;
-  //   if(dataRemote>0){
-  //     Serial.println(dataRemote);}}
-  if (irrecv.decode(&results))
-  {
-    unsigned long value = results.value;
-    Serial.println("-------");
-    Serial.print("Mã nhận được: ");
-    // Serial.println(value, HEX);
-    Serial.println(value, DEC);  // Hiển thị giá trị dưới dạng số thập phân
-    // Serial.println(value, BIN);  // Hiển thị giá trị dưới dạng số nhị phân
-    // Serial.println(value, OCT);  // Hiển thị giá trị dưới dạng số bát phân
-    // Serial.println(value, BYTE); // Hiển thị giá trị dưới dạng số nguyên 8-bit
-
-    irrecv.resume();  // Tiếp tục nhận tín hiệu hồng ngoại
-  }
+  
+  // if (irrecv.decode(&results))
+  // {
+  //   unsigned long value = results.value;
+  //   // Serial.println(value, DEC);
+  //   Serial.println(value);
+  //   irrecv.resume();
+  //   if (value=4321&&millis()-lastTime>3000)
+  //   {
+  //     digitalWrite(coi, 1);
+  //     digitalWrite(denDo, 1);
+  //     digitalWrite(denXanh, 0);
+  //     digitalWrite(denVang, 0);
+  //     lastTime=millis();
+  //   } 
+  // }
+  xanh1();
+  irsend.sendSony(4321, 20);
+  delay(100);
+  // if (millis() - lastTime > 500)
+  // {
+  //   digitalWrite(coi, 0); 
+  // }
+  // if (millis() - lastTime > 2000)
+  // {
+  //   digitalWrite(coi, 0);
+  //   digitalWrite(denDo, 0);
+  //   digitalWrite(denXanh, 1);
+  //   digitalWrite(denVang, 0);
+  // }
+  
 }
